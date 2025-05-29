52516c1614e267ab91205d23f5d10fe535083e58
Ashfaq Khan <82702762+ashfaqrafin@users.noreply.github.com>
2025-05-29 19:09:30 +0600

    modular arithmatic

---
+++ mod.cpp
@@ -0,0 +1,6 @@
+int power_mod(int v, int n) {v %= mod;if(v == 0) return 0;n %= (mod - 1); int ret = 1;for(; n; n >>= 1, v = (v * v) % mod) if(n & 1) ret = (ret * v) % mod;return ret;}
+int mod_sub(int x, int y) {x %= mod; y %= mod;return (x + mod - y) % mod;}
+int mod_add(int x, int y) {x %= mod; y %= mod;return (x + y) % mod;}
+int mod_mul(int x, int y) {x %= mod; y %= mod;return (x * y) % mod;}
+int mod_inv(int x) {x %= mod;return power_mod(x, mod - 2);}
+int mod_div(int x, int y) {x %= mod; y %= mod;return mod_mul(x, mod_inv(y));}
